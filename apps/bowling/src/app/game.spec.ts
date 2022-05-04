b55fce8b6bdbf27c9195645680dac5220e69c1dd
ChaShaoAn <kevinchao840713@gmail.com>
2022-05-04 20:56:22 +0800

    second green

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -11,6 +11,14 @@ describe('Game', () => {
       game.roll(0);
     }
     expect(game.score).toBe(0);
-});
+  });
+
+  test('all one', () => {
+    const game = new Game();
+    for (let i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score).toBe(20);
+  });
 
 });
